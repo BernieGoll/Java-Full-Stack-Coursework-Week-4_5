c4e392c1fcb611d3113c6193c87c1e38f63cfbcc
BernieG <berniegoll@gmail.com>
2017-12-17 15:54:15 -0600

    adding files from partial Phone Book Project

---
+++ phnBk/Application.java
@@ -0,0 +1,48 @@
+// main class for phone book application
+package phnBk;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Application
+{
+	
+	public static void main(String[]args)
+	{
+		Scanner abc = new Scanner(System.in);
+		
+		String contact1 = new String("John Doe, 114 Market St, St Louis, MO, 63403, 6366435698");
+		String contact2 = new String("John E Doe, 324 Main St, St Charles, MO, 63303, 8475390126");
+		String contact3 = new String("John Michael West Doe, 574 Pole Ave, St Peters, MO, 63333, 5628592375");						
+				
+		System.out.println("Choose your Option by Number:");
+		System.out.println("1. Call Contact");
+		System.out.println("2. Add A Contact");
+		System.out.println("3. Delete A Contact");
+		System.out.println("4. Search for Contact");
+		System.out.println("5. Search by First Name");
+		System.out.println("6. Search by Last Name");
+		System.out.println("7. Search by Telephone #");
+		System.out.println("8. Search by City or State");
+		System.out.println("9. Exit Program");				
+		
+		
+		
+		{
+			for(int i= 0; i <=test1.length() - 2; i++);
+			
+		}
+		
+
+	/*do
+	{
+		if()
+			
+			
+	}
+	while()		
+	*/					
+		
+	}	
+	
+}
---
+++ phnBk/contact.java
@@ -0,0 +1,41 @@
+// class for contact object for phoneBook app
+package phnBk;
+
+public class contact
+{
+	public contact()
+	{
+		
+	}
+	private String firstName;
+	private String lastName;
+	private long phoneN;
+	public void addressPBk()
+	{
+		
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getPhoneN() {
+		return phoneN;
+	}
+	public void setPhoneN(long phoneN) {
+		this.phoneN = phoneN;
+	}
+	
+
+
+	
+}
